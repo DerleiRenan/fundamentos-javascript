9baf8b4f0014d37b69cea28d619225b31476fd51
Derlei Renan <derlei_santos@hotmail.com>
2021-11-26 11:50:56 -0300

    Exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+//Ponto Flutuante
+
+const numeroPontoFLutuante = 3.3;
+const numeroFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFLutuante;
+console.log(novaOperacao);
+
+//NaN -> Not a bumber (Não é um numero)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
+
